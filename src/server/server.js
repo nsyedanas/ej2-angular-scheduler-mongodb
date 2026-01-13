1a9b48eaed9ec479abb71bb23c99ff8d91550167
Syed Anas Nizam Deen <syedanas.nizamdeen@gmail.com>
2026-01-13 18:36:52 +0530

    updated mongodb version to 7

--- src/server/server.js
+++ src/server/server.js
@@ -1,55 +1,75 @@
 var MongoClient = require('mongodb').MongoClient;
 const express = require('express');
-const bodyParser = require('body-parser');
+var cors = require('cors');
 const app = express();
 var url = "mongodb://localhost:27017/";
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.listen(5000, function () {
-    console.log('listening on 5000')
-})
-app.use(express.static(__dirname))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
-    app.post("/GetData", (req, res) => {
-        dbo.collection('ScheduleData').find({}).toArray((err, cus) => {
-            res.send(cus);
+app.use(cors({
+    origin: 'http://localhost:4200', // your React dev server
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false // set to true ONLY if you use cookies/Authorization with cross-origin
+}));
+
+app.use(express.static(__dirname));
+app.listen(5000, function () { console.log('listening on 5000'); });
+
+(async () => {
+    try {
+        const client = new MongoClient(url);
+        await client.connect();
+        const dbo = client.db('mydb');
+
+        app.post("/GetData", async function (req, res) {
+            try {
+                const cus = await dbo.collection('ScheduleData').find({}).toArray();
+                res.json(cus);
+            } catch (err) {
+                res.status(500).json({ error: 'DB error', details: err.message });
+            }
         });
-    });
-    
-    app.post("/BatchData", (req, res) => {
-        console.log(req.body);
-        var eventData = [];
-        if (req.body.action == "insert" || (req.body.action == "batch" && req.body.added.length > 0)) {
-            (req.body.action == "insert") ? eventData.push(req.body.value) : eventData = req.body.added;
-            for (var i = 0; i < eventData.length; i++) {
-                eventData[i].StartTime = new Date(eventData[i].StartTime);
-                eventData[i].EndTime = new Date(eventData[i].EndTime);
-                dbo.collection('ScheduleData').insertOne(eventData[i]);
+        app.post("/BatchData", async function (req, res) {
+            try {
+                let eventData = [];
+                if (req.body.action === "insert" || (req.body.action === "batch" && req.body.added && req.body.added.length > 0)) {
+                    (req.body.action === "insert") ? eventData.push(req.body.value) : eventData = req.body.added;
+                    for (let a = 0; a < eventData.length; a++) {
+                        eventData[a].StartTime = new Date(eventData[a].StartTime);
+                        eventData[a].EndTime = new Date(eventData[a].EndTime);
+                        await dbo.collection('ScheduleData').insertOne(eventData[a]);
+                    }
+                }
+                if (req.body.action === "update" || (req.body.action === "batch" && req.body.changed && req.body.changed.length > 0)) {
+                    (req.body.action === "update") ? eventData.push(req.body.value) : eventData = req.body.changed;
+                    for (let b = 0; b < eventData.length; b++) {
+                        delete eventData[b]._id;
+                        eventData[b].StartTime = new Date(eventData[b].StartTime);
+                        eventData[b].EndTime = new Date(eventData[b].EndTime);
+                        await dbo.collection('ScheduleData').updateOne({ "Id": eventData[b].Id }, { $set: eventData[b] });
+                    }
+                }
+                if (req.body.action === "remove" || (req.body.action === "batch" && req.body.deleted && req.body.deleted.length > 0)) {
+                    (req.body.action === "remove") ? eventData.push({ Id: req.body.key }) : eventData = req.body.deleted;
+                    for (let c = 0; c < eventData.length; c++) {
+                        await dbo.collection('ScheduleData').deleteOne({ "Id": eventData[c].Id });
+                    }
+                }
+                res.json(req.body);
+            } catch (err) {
+                res.status(500).json({ error: 'DB error', details: err.message });
             }
-        }
-        if (req.body.action == "update" || (req.body.action == "batch" && req.body.changed.length > 0)) {
-            (req.body.action == "update") ? eventData.push(req.body.value) : eventData = req.body.changed;
-            for (var i = 0; i < eventData.length; i++) {
-                delete eventData[i]._id;
-                eventData[i].StartTime = new Date(eventData[i].StartTime);
-                eventData[i].EndTime = new Date(eventData[i].EndTime);
-                dbo.collection('ScheduleData').updateOne({ "Id": eventData[i].Id }, { $set: eventData[i] });
-            }
-        }
-        if (req.body.action == "remove" || (req.body.action == "batch" && req.body.deleted.length > 0)) {
-            (req.body.action == "remove") ? eventData.push({ Id: req.body.key }) : eventData = req.body.deleted;
-            for (var i = 0; i < eventData.length; i++) {
-                dbo.collection('ScheduleData').deleteOne({ "Id": eventData[i].Id });
-            }
-        }
-        res.send(req.body);
-    });
-});
+        });
+
+        // Optional: handle SIGINT to close client
+        process.on('SIGINT', async () => {
+            await client.close();
+            process.exit(0);
+        });
+
+    } catch (err) {
+        console.error('Mongo connection failed:', err);
+        process.exit(1);
+    }
+})();
